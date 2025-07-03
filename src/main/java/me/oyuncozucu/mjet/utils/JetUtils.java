1ce00527be7a1c03236bc7d421ef1931f52641a6
Tahsin <75451952+bentahsin@users.noreply.github.com>
2025-07-03 03:07:50 +0300

    Update JetUtils.java
    XSeries Integration

--- src/main/java/me/oyuncozucu/mjet/utils/JetUtils.java
+++ src/main/java/me/oyuncozucu/mjet/utils/JetUtils.java
@@ -1,19 +1,25 @@
 package me.oyuncozucu.mjet.utils;
 
+import com.cryptomorin.xseries.XMaterial; // İçe aktar
 import me.oyuncozucu.mjet.MJet;
 import org.bukkit.ChatColor;
-import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.Optional;
 
 public class JetUtils {
 
     public static ItemStack isJet() {
+        ItemStack jet = XMaterial.FIREWORK_ROCKET.parseItem();
 
-        ItemStack jet = new ItemStack(Material.FIREWORK_ROCKET);
+        if (jet == null) {
+            return null;
+        }
+
         ItemMeta jetMeta = jet.getItemMeta();
         String displayName = MJet.getInstance().getConfig().getString("item-name");
-        jetMeta.setDisplayName(ChatColor.translateAlternateColorCodes('&',displayName));
+        jetMeta.setDisplayName(ChatColor.translateAlternateColorCodes('&', displayName));
         jet.setItemMeta(jetMeta);
 
         return jet;
